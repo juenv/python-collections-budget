817d53da2f979793453a0c7afa919ae710853f4b
Juha-Matti Ensio Valjus <jvalj@nets.eu>
2022-02-25 08:02:09 -0800

    Count Expenses by Category revised

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -12,10 +12,10 @@ for expense in expenses.list:
 
 spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
-categories, counts = zip(*top5)
+categories, count = zip(*top5)
 
 fig, ax = plt.subplots()
-ax.bar(categories, counts)
+ax.bar(categories, count)
 ax.set_title('# of Purchases by Category')
 
 plt.show()
